58c2ad50e89cdd0c9acf5590ea5991f69b7e6f73
TAWEESOFT <taweesoft@gmail.com>
2016-05-13 21:15:35 +0700

    [#119505445] Changed @Body from User to RequestBody

--- app/src/main/java/io/joinpa/joinpa/models/Token.java
+++ app/src/main/java/io/joinpa/joinpa/models/Token.java
@@ -1,9 +1,12 @@
 package io.joinpa.joinpa.models;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by TAWEESOFT on 5/13/16 AD.
  */
 public class Token {
+    @SerializedName("key")
     private String key;
 
     public Token(String key) {
