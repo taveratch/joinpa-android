55c05055a35436ebca6244a3385f5f2277a57d3f
TAWEESOFT <taweesoft@gmail.com>
2016-05-13 14:07:08 +0700

    Added directories for classes

--- app/src/main/java/io/joinpa/joinpa/ui/views/MainActivity.java
+++ app/src/main/java/io/joinpa/joinpa/ui/views/MainActivity.java
@@ -1,7 +1,9 @@
-package io.joinpa.joinpa;
+package io.joinpa.joinpa.ui.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import io.joinpa.joinpa.R;
 
 public class MainActivity extends AppCompatActivity {
 
