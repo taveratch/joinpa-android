5ffc3d7b2677c54737473c7cc8cf54cbc5792edf
Thongrapee <kulaspa@gmail.com>
2016-05-13 15:17:06 +0700

    Layout draft

--- app/src/main/java/io/joinpa/joinpa/ui/views/SignUpActivity.java
+++ app/src/main/java/io/joinpa/joinpa/ui/views/SignUpActivity.java
@@ -2,12 +2,26 @@ package io.joinpa.joinpa.ui.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.joinpa.joinpa.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    @BindView(R.id.choose_avatar_view)
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(llm);
     }
 }
