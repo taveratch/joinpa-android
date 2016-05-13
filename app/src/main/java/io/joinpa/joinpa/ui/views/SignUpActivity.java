77982aa455bc69cae76a9f07a09fcae99644fb97
Thongrapee <kulaspa@gmail.com>
2016-05-13 15:56:34 +0700

    Change layouts in input form

--- app/src/main/java/io/joinpa/joinpa/ui/views/SignUpActivity.java
+++ app/src/main/java/io/joinpa/joinpa/ui/views/SignUpActivity.java
@@ -1,9 +1,12 @@
 package io.joinpa.joinpa.ui.views;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +17,30 @@ public class SignUpActivity extends AppCompatActivity {
     @BindView(R.id.choose_avatar_view)
     RecyclerView recyclerView;
 
+    @BindView(R.id.username_wrapper)
+    TextInputLayout usernameWrapper;
+
+    @BindView(R.id.username_input)
+    EditText usernameInput;
+
+    @BindView(R.id.password_wrapper)
+    TextInputLayout passwordWrapper;
+
+    @BindView(R.id.password_input)
+    EditText passwordInput;
+
+    @BindView(R.id.cpassword_wrapper)
+    TextInputLayout cpasswordWrapper;
+
+    @BindView(R.id.cpassowrd_input)
+    EditText cpasswordInput;
+
+    @BindView(R.id.email_wrapper)
+    TextInputLayout emailWrapper;
+
+    @BindView(R.id.email_input)
+    EditText emailInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +49,11 @@ public class SignUpActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(llm);
+//        recyclerView.setLayoutManager(llm);
+
+        // Set EditText to password field input
+//        passwordInput.setTransformationMethod(new PasswordTransformationMethod());
+//        cpasswordInput.setTransformationMethod(new PasswordTransformationMethod());
+
     }
 }
