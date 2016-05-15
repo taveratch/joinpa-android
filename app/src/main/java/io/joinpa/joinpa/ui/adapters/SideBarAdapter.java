c955d2581b556e37bf5a045bb805826c991699c2
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-05-15 11:54:07 +0700

    [#119590741] Added toolbar style and use toolbar in main activity

---
+++ app/src/main/java/io/joinpa/joinpa/ui/adapters/SideBarAdapter.java
@@ -0,0 +1,55 @@
+package io.joinpa.joinpa.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.joinpa.joinpa.R;
+import io.joinpa.joinpa.models.SideBarItem;
+
+/**
+ * Created by TAWEESOFT on 5/15/16 AD.
+ */
+public class SideBarAdapter extends ArrayAdapter<SideBarItem> {
+
+    public SideBarAdapter(Context context, int resource, List<SideBarItem> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.sidebar_item_layout,null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        SideBarItem item = getItem(position);
+        holder.imgIcon.setImageResource(item.getIcon());
+        holder.tvItemName.setText(item.getTitle());
+        return convertView;
+    }
+
+    class ViewHolder {
+        @BindView(R.id.img_icon)
+        ImageView imgIcon;
+
+        @BindView(R.id.tv_itemname)
+        TextView tvItemName;
+
+        public ViewHolder(View v) {
+            ButterKnife.bind(this,v);
+        }
+    }
+}
--- app/src/main/java/io/joinpa/joinpa/ui/views/MainActivity.java
+++ app/src/main/java/io/joinpa/joinpa/ui/views/MainActivity.java
@@ -2,20 +2,31 @@ package io.joinpa.joinpa.ui.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
 
 import net.simonvt.menudrawer.MenuDrawer;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.joinpa.joinpa.R;
 import io.joinpa.joinpa.models.SideBarItem;
+import io.joinpa.joinpa.ui.adapters.SideBarAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.lv_sidebar)
+    ListView lvSideBar;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     private MenuDrawer menuDrawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +39,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initComponents() {
+        List<SideBarItem> sideBarItems = new ArrayList<>();
+        sideBarItems.add(new SideBarItem(getString(R.string.explore) , R.drawable.sidebar_explore_icon));
+        sideBarItems.add(new SideBarItem(getString(R.string.my_events) , R.drawable.sidebar_myevent_icon));
+        sideBarItems.add(new SideBarItem(getString(R.string.invites) , R.drawable.sidebar_invites_icon));
+        sideBarItems.add(new SideBarItem(getString(R.string.recent_events) , R.drawable.sidebar_recent_icon));
+        sideBarItems.add(new SideBarItem(getString(R.string.friends) , R.drawable.sidebar_friend_icon));
+
+        SideBarAdapter sideBarAdapter = new SideBarAdapter(this,R.layout.sidebar_item_layout , sideBarItems);
+        lvSideBar.setAdapter(sideBarAdapter);
+
+        toolbar.setNavigationIcon(R.drawable.toolbar_menu_button);
+        toolbar.setTitle("Main menu");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuDrawer.toggleMenu(true);
+            }
+        });
+
+        // TODO: 5/15/16 AD Use fragment in Main activity 
     }
 }
