a5c1cbd09062202064c31f5360ebddd5210a3541
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-05-15 11:33:16 +0700

    [#119590741] Added menu drawer library and layout

--- app/src/main/java/io/joinpa/joinpa/ui/views/MainActivity.java
+++ app/src/main/java/io/joinpa/joinpa/ui/views/MainActivity.java
@@ -3,13 +3,30 @@ package io.joinpa.joinpa.ui.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.simonvt.menudrawer.MenuDrawer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
 import io.joinpa.joinpa.R;
+import io.joinpa.joinpa.models.SideBarItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private MenuDrawer menuDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        menuDrawer = MenuDrawer.attach(this);
+        menuDrawer.setContentView(R.layout.activity_main);
+        menuDrawer.setMenuView(R.layout.sidebar_layout);
+        ButterKnife.bind(menuDrawer);
+        ButterKnife.bind(this);
+        initComponents();
+    }
+
+    public void initComponents() {
     }
 }
