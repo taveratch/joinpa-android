fc0d97fff0775315df2cbdda24ad3de56ea49382
Thongrapee <kulaspa@gmail.com>
2016-05-20 13:55:31 +0700

    Add selectable recyclerview library

--- app/src/main/java/io/joinpa/joinpa/ui/adapters/UserAvatarAdapter.java
+++ app/src/main/java/io/joinpa/joinpa/ui/adapters/UserAvatarAdapter.java
@@ -2,11 +2,16 @@ package io.joinpa.joinpa.ui.adapters;
 
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bignerdranch.android.multiselector.MultiSelector;
+import com.bignerdranch.android.multiselector.SingleSelector;
+import com.bignerdranch.android.multiselector.SwappingHolder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,16 +20,32 @@ import io.joinpa.joinpa.R;
 
 public class UserAvatarAdapter extends RecyclerView.Adapter<UserAvatarAdapter.ViewHolder> {
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    private static MultiSelector selector = new SingleSelector();
+    private static int selectedAvatar = 0;
+
+    public static class ViewHolder extends SwappingHolder implements View.OnClickListener {
 
         @BindView(R.id.avatar_image_view)
         ImageView avatar;
 
         public ViewHolder(View v) {
-            super(v);
+            super(v, selector);
+            v.setOnClickListener(this);
             ButterKnife.bind(this, v);
         }
 
+
+        @Override
+        public void onClick(View v) {
+            selectedAvatar = getAdapterPosition();
+            selector.setSelectable(true); // enter selection mode
+            selector.setSelected(this, true); // set myViewHolder to selected
+            Log.e("Click", "Clicked on " + getAdapterPosition());
+        }
+    }
+
+    public int getSelectedAvatar() {
+        return selectedAvatar;
     }
 
     @Override
