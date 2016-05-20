c2a332f2d4d79de3a748540ceedc31c083255327
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-05-20 15:29:14 +0700

    [#119977243] Added friend list layout with fake data

--- app/src/main/java/io/joinpa/joinpa/ui/fragments/FriendFragment.java
+++ app/src/main/java/io/joinpa/joinpa/ui/fragments/FriendFragment.java
@@ -2,14 +2,22 @@ package io.joinpa.joinpa.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.joinpa.joinpa.R;
+import io.joinpa.joinpa.models.Friend;
+import io.joinpa.joinpa.ui.adapters.FriendListAdapter;
 
 /**
  * Created by TAWEESOFT on 5/15/16 AD.
@@ -18,6 +26,12 @@ public class FriendFragment extends ObservableFragment {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.et_search)
+    EditText etSearch;
+
+    @BindView(R.id.rv)
+    RecyclerView rv;
 
     @Nullable
     @Override
@@ -29,6 +43,20 @@ public class FriendFragment extends ObservableFragment {
     }
 
     public void initComponents() {
+
+        List<Friend> friendList = new ArrayList<>();
+        Friend f1 = new Friend();
+        f1.setUsername("tonyslark");
+        Friend f2 = new Friend();
+        f2.setUsername("jason.b");
+        Friend f3 = new Friend();
+        f3.setUsername("freddy.c");
+        friendList.add(f1);
+        friendList.add(f2);
+        friendList.add(f3);
+        FriendListAdapter adapter = new FriendListAdapter(this.getContext(),friendList);
+        rv.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        rv.setAdapter(adapter);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
